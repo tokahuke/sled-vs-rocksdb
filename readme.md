77aaf357181a81edad39f9554be9433a22896fc2
Pedro Bittencorut Arruda <pedrobittencourt@yahoo.com.br>
2019-07-06 01:35:09 -0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Toy benchmark comparing RocksDB and Sled
+
+I have created a sketch of a benchmark of RocksDB and Sled trying to decide whether I should use Sled instead of RocksDB for a new project I am developing. From the start, I like more the API of Sled (and its compilation time). However, RocksDB still seems to be the better alternative as of this date. Hopefully, as Sled stabilizes, these conclusions might be different.
+
+## Workload
+
+This is a workload that somewhat resembles my intended usecase: populate the databse with many keys, each associated with a list, to which new elements ca be pushed (but not deleted). I have populated the both Sled and RocksDB with integer 32-bit keys ranging from 0 to 1mi and merged the 32-bit numbers from 0 to 10
+
+## Running it
+
+I have used `jemallocator` and `lto=thin` to get the numbers below. Both database are using compression: RocksDB is using LZ4 and Sled has the `use_compression` enabled. I have also used `cargo flamegraph`, recommended by the author. I have tested them in a Ubuntu 18 Intel i5 machine with an HD drive and 8GB of RAM.
+
+### Results
+
+Here is a sample run. I have made many other runs and thre results have been roughly the same:
+
+|         | merging load | iterating load | disk usage |
+|      ---|           ---|             ---|         ---|
+| RocksDB |     100s     |      0.85s     |    34MB    |
+| Sled    |     207s     |      1.85s     |   226MB    |
+
+
+RocksDB is still roughly twice as fast as Sled and beats Sled on disk usage. That number has been consistent through all runs. I have noticed that these number become more even when the number of keys is some orders of magnitude smaller, but the number of keys I have proposed is already on the small size of many applications.
+
+For this particular run, I have also got the following flamegraph:
+
+![flamegraph](flamegraph-1.svg)
+
+## Questions
+
+- Should these numbers be wildly different in SSD? My computer still uses HD.
+- Is this workload really that realistic? It is near to what I intend to use (merging and scanning).
+- Have I configured the system the right way? Anything that could have made both databses perform wildly better?
---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use std::time::Instant;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+/// Path of Sled DB in disk.
+pub const SLED_PATH: &str = "./sled";
+
+/// Path to RocksDB in disk.
+pub const ROCKS_PATH: &str = "./rocks";
+
+/// This is the concatenation merge operator in Sled.
+fn sled_cat(_key: &[u8], val: Option<&[u8]>, new: &[u8]) -> Option<Vec<u8>> {
+    Some(val.into_iter().flatten().chain(new).cloned().collect())
+}
+
+/// This is the concatenation merge operator in RocksDB.
+fn rocks_cat(_key: &[u8], val: Option<&[u8]>, new: &mut rocksdb::MergeOperands) -> Option<Vec<u8>> {
+    Some(
+        val.into_iter()
+            .flatten()
+            .chain(new.into_iter().flatten())
+            .cloned()
+            .collect(),
+    )
+}
+
+/// Quick and dirty slice to u32.
+fn from_bytes(b: &[u8]) -> u32 {
+    u32::from_le_bytes([b[0], b[1], b[2], b[3]])
+}
+
+fn main() {
+    // This is how we initialize Sled.
+    let sled_db = {
+        let config = sled::ConfigBuilder::default()
+            .merge_operator(sled_cat)
+            .path(SLED_PATH)
+            .use_compression(true);
+
+        sled::Db::start(config.build()).unwrap()
+    };
+
+    // This is how we initialize RocksDB.
+    let rocks_db = {
+        let mut options = rocksdb::Options::default();
+        options.create_if_missing(true);
+        options.set_merge_operator("rocks_cat", rocks_cat, None);
+        options.set_compression_type(rocksdb::DBCompressionType::Lz4);
+
+        rocksdb::DB::open(&options, ROCKS_PATH).unwrap()
+    };
+
+    // 1. Fill each DB with consecutive integers, all holding ntegers from 0 to 9 concatenated.
+
+    // This is how we do it in Sled.
+    let tic = Instant::now();
+
+    for i in 0..1_000_000u32 {
+        for j in 0..10u32 {
+            sled_db.merge(&i.to_le_bytes(), &j.to_le_bytes()).unwrap();
+        }
+    }
+
+    println!("Sled: {:?}", tic.elapsed());
+
+    // This is how we do it in RocksDB.
+    let tic = Instant::now();
+    for i in 0..1_000_000u32 {
+        for j in 0..10u32 {
+            rocks_db.merge(&i.to_le_bytes(), &j.to_le_bytes()).unwrap();
+        }
+    }
+
+    println!("RocksDB: {:?}", tic.elapsed());
+
+    // 2. Now, sum all integers contained in all keys.
+
+    // This is how we do it in Sled.
+    let tic = Instant::now();
+    let count = sled_db
+        .iter()
+        .map(Result::unwrap)
+        .map(|(_, val)| val.as_ref().windows(4).map(from_bytes).collect::<Vec<_>>())
+        .flatten()
+        .map(|i| i as u64)
+        .sum::<u64>();
+    dbg!(count);
+
+    println!("Sled: {:?}", tic.elapsed());
+
+    // This is how we do it in RocksDB.
+    let tic = Instant::now();
+    let count = rocks_db
+        .iterator(rocksdb::IteratorMode::Start)
+        .map(|(_, val)| val.as_ref().windows(4).map(from_bytes).collect::<Vec<_>>())
+        .flatten()
+        .map(|i| i as u64)
+        .sum::<u64>();
+    dbg!(count);
+
+    println!("RocksDB: {:?}", tic.elapsed());
+}
