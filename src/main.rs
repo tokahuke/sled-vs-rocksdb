38d42db0fb6429ae5cab1c9611c6925cab1b10ca
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-08-27 04:42:11 -0700

    feat: separate the backends into features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
 
+#[cfg(feature = "jemallocator")]
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
@@ -10,11 +11,13 @@ pub const SLED_PATH: &str = "./sled";
 pub const ROCKS_PATH: &str = "./rocks";
 
 /// This is the concatenation merge operator in Sled.
+#[cfg(feature = "sled")]
 fn sled_cat(_key: &[u8], val: Option<&[u8]>, new: &[u8]) -> Option<Vec<u8>> {
     Some(val.into_iter().flatten().chain(new).cloned().collect())
 }
 
 /// This is the concatenation merge operator in RocksDB.
+#[cfg(feature = "rocksdb")]
 fn rocks_cat(_key: &[u8], val: Option<&[u8]>, new: &mut rocksdb::MergeOperands) -> Option<Vec<u8>> {
     Some(
         val.into_iter()
@@ -30,7 +33,8 @@ fn from_bytes(b: &[u8]) -> u32 {
     u32::from_le_bytes([b[0], b[1], b[2], b[3]])
 }
 
-fn main() {
+#[cfg(feature = "sled")]
+fn sled_main() {
     // This is how we initialize Sled.
     let sled_db = {
         let config = sled::Config::default()
@@ -39,21 +43,11 @@ fn main() {
             .cache_capacity(1_000_000_000)
             .flush_every_ms(Some(200))
             .print_profile_on_drop(true);
-        
+
         let db = config.open().unwrap();
         db.set_merge_operator(sled_cat);
 
         db
-    };
-
-    // This is how we initialize RocksDB.
-    let rocks_db = {
-        let mut options = rocksdb::Options::default();
-        options.create_if_missing(true);
-        options.set_merge_operator("rocks_cat", rocks_cat, None);
-        options.set_compression_type(rocksdb::DBCompressionType::Lz4);
-
-        rocksdb::DB::open(&options, ROCKS_PATH).unwrap()
     };
 
     // 1. Fill each DB with consecutive integers, all holding ntegers from 0 to 9 concatenated.
@@ -69,16 +63,6 @@ fn main() {
 
     println!("Sled: {:?}", tic.elapsed());
 
-    // This is how we do it in RocksDB.
-    let tic = Instant::now();
-    for i in 0..1_000_000u32 {
-        for j in 0..10u32 {
-            rocks_db.merge(&i.to_le_bytes(), &j.to_le_bytes()).unwrap();
-        }
-    }
-
-    println!("RocksDB: {:?}", tic.elapsed());
-
     // 2. Now, sum all integers contained in all keys.
 
     // This is how we do it in Sled.
@@ -93,6 +77,33 @@ fn main() {
     dbg!(count);
 
     println!("Sled: {:?}", tic.elapsed());
+}
+
+#[cfg(feature = "rocksdb")]
+fn rocksdb_main() {
+    // This is how we initialize RocksDB.
+    let rocks_db = {
+        let mut options = rocksdb::Options::default();
+        options.create_if_missing(true);
+        options.set_merge_operator("rocks_cat", rocks_cat, None);
+        options.set_compression_type(rocksdb::DBCompressionType::Lz4);
+
+        rocksdb::DB::open(&options, ROCKS_PATH).unwrap()
+    };
+
+    // 1. Fill each DB with consecutive integers, all holding ntegers from 0 to 9 concatenated.
+
+    // This is how we do it in RocksDB.
+    let tic = Instant::now();
+    for i in 0..1_000_000u32 {
+        for j in 0..10u32 {
+            rocks_db.merge(&i.to_le_bytes(), &j.to_le_bytes()).unwrap();
+        }
+    }
+
+    println!("RocksDB: {:?}", tic.elapsed());
+
+    // 2. Now, sum all integers contained in all keys.
 
     // This is how we do it in RocksDB.
     let tic = Instant::now();
@@ -105,4 +116,16 @@ fn main() {
     dbg!(count);
 
     println!("RocksDB: {:?}", tic.elapsed());
+}
+
+fn main() {
+    #[cfg(feature = "sled")]
+    {
+        sled_main();
+    }
+
+    #[cfg(feature = "rocksdb")]
+    {
+        rocksdb_main();
+    }
 }
